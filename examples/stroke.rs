3752d28280934c21eec31b14c59cb70f9574d6e7
Felix Zwettler <f.zwettler@posteo.de>
2023-05-08 11:44:46 +0200

    update ink-stroke-modeler, improve example

--- examples/stroke.rs
+++ examples/stroke.rs
@@ -8,14 +8,21 @@ use svg::Node;
 fn main() -> anyhow::Result<()> {
     let bounds = AABB {
         mins: (0.0, 0.0),
-        maxs: (6.0, 4.0),
+        maxs: (300.0, 300.0),
     };
 
-    let stroke = vec![
-        ModelerInput::new(ModelerInputEventType::kDown, (1.0, 1.0), 0.0, 0.1, 0.0, 0.0),
+    let input_stroke = vec![
+        ModelerInput::new(
+            ModelerInputEventType::kDown,
+            (90.0, 30.0),
+            0.0,
+            0.25,
+            0.0,
+            0.0,
+        ),
         ModelerInput::new(
             ModelerInputEventType::kMove,
-            (1.2, 2.0),
+            (30.0, 45.0),
             0.02,
             0.3,
             0.0,
@@ -23,51 +30,90 @@ fn main() -> anyhow::Result<()> {
         ),
         ModelerInput::new(
             ModelerInputEventType::kMove,
-            (1.4, 2.3),
+            (60.0, 240.0),
             0.04,
-            0.5,
+            0.7,
             0.0,
             0.0,
         ),
         ModelerInput::new(
             ModelerInputEventType::kMove,
-            (3.5, 2.0),
+            (105.0, 270.0),
             0.06,
-            0.8,
+            1.0,
             0.0,
             0.0,
         ),
         ModelerInput::new(
             ModelerInputEventType::kMove,
-            (4.0, 2.5),
+            (120.0, 150.0),
+            0.10,
+            0.6,
+            0.0,
+            0.0,
+        ),
+        ModelerInput::new(
+            ModelerInputEventType::kMove,
+            (180.0, 30.0),
             0.12,
+            0.3,
+            0.0,
+            0.0,
+        ),
+        ModelerInput::new(
+            ModelerInputEventType::kMove,
+            (240.0, 120.0),
+            0.16,
+            0.3,
+            0.0,
+            0.0,
+        ),
+        ModelerInput::new(
+            ModelerInputEventType::kMove,
+            (210.0, 150.0),
+            0.18,
             0.9,
             0.0,
             0.0,
         ),
         ModelerInput::new(
             ModelerInputEventType::kMove,
-            (5.0, 3.0),
-            0.13,
+            (150.0, 210.0),
+            0.20,
             0.8,
             0.0,
             0.0,
         ),
         ModelerInput::new(
             ModelerInputEventType::kMove,
-            (4.8, 3.1),
-            0.13,
+            (210.0, 240.0),
+            0.22,
+            0.8,
+            0.0,
+            0.0,
+        ),
+        ModelerInput::new(
+            ModelerInputEventType::kMove,
+            (255.0, 240.0),
+            0.24,
             0.7,
             0.0,
             0.0,
         ),
-        ModelerInput::new(ModelerInputEventType::kUp, (4.5, 3.0), 0.14, 0.2, 0.0, 0.0),
+        ModelerInput::new(
+            ModelerInputEventType::kUp,
+            (270.0, 270.0),
+            0.26,
+            0.5,
+            0.0,
+            0.0,
+        ),
     ];
-    let input_elements = stroke
+    let input_elements = input_stroke
         .iter()
         .map(Element::from_modeler_input)
         .collect::<Vec<Element>>();
-    Element::create_svg(
+    create_svg(
         &input_elements,
         bounds,
         std::path::PathBuf::from("./examples/stroke/input.svg"),
@@ -75,7 +121,7 @@ fn main() -> anyhow::Result<()> {
 
     let mut modeler = StrokeModeler::default();
 
-    let results = stroke
+    let result_stroke = input_stroke
         .into_iter()
         .filter_map(|i| {
             modeler
@@ -86,14 +132,14 @@ fn main() -> anyhow::Result<()> {
         .flatten()
         .collect::<Vec<ModelerResult>>();
 
-    let result_elements = results
+    let result_elements = result_stroke
         .iter()
         .map(Element::from_modeler_result)
         .collect::<Vec<Element>>();
-    Element::create_svg(
+    create_svg(
         &result_elements,
         bounds,
-        std::path::PathBuf::from("./examples/stroke/results.svg"),
+        std::path::PathBuf::from("./examples/stroke/modeled.svg"),
     )?;
 
     Ok(())
@@ -131,42 +177,6 @@ impl Element {
             orientation: r.get_orientation(),
         }
     }
-
-    fn create_svg(
-        elements: &[Self],
-        bounds: AABB,
-        file: impl AsRef<std::path::Path>,
-    ) -> anyhow::Result<()> {
-        let mut doc = svg::Document::new()
-            .set("x", bounds.mins.0)
-            .set("y", bounds.mins.1)
-            .set("width", bounds.width())
-            .set("height", bounds.height());
-
-        doc.append(
-            svg::node::element::Rectangle::new()
-                .set("x", bounds.mins.0)
-                .set("y", bounds.mins.1)
-                .set("width", bounds.width())
-                .set("height", bounds.height())
-                .set("fill", "white"),
-        );
-
-        for (start, end) in elements.iter().zip(elements.iter().skip(1)) {
-            doc.append(
-                svg::node::element::Line::new()
-                    .set("x1", start.pos.0)
-                    .set("y1", start.pos.1)
-                    .set("x2", end.pos.0)
-                    .set("y2", end.pos.1)
-                    //.set("stroke-width", (end.pressure + start.pressure) / 2.0)
-                    .set("stroke-width", 0.01)
-                    .set("stroke", "black"),
-            );
-        }
-
-        Ok(svg::save(file, &doc)?)
-    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -196,4 +206,44 @@ impl AABB {
         self.maxs.0 = self.maxs.0.max(coord.0);
         self.maxs.1 = self.maxs.1.max(coord.1);
     }
+}
+
+fn create_svg(
+    elements: &[Element],
+    bounds: AABB,
+    file: impl AsRef<std::path::Path>,
+) -> anyhow::Result<()> {
+    let mut doc = svg::Document::new()
+        .set("x", bounds.mins.0)
+        .set("y", bounds.mins.1)
+        .set("width", bounds.width())
+        .set("height", bounds.height());
+
+    doc.append(
+        svg::node::element::Rectangle::new()
+            .set("x", bounds.mins.0)
+            .set("y", bounds.mins.1)
+            .set("width", bounds.width())
+            .set("height", bounds.height())
+            .set("fill", "white"),
+    );
+
+    for (start, end) in elements.iter().zip(elements.iter().skip(1)) {
+        let brightness = 1.0 / (end.pressure + start.pressure) / 2.0;
+        doc.append(
+            svg::node::element::Line::new()
+                .set("x1", start.pos.0)
+                .set("y1", start.pos.1)
+                .set("x2", end.pos.0)
+                .set("y2", end.pos.1)
+                .set(
+                    "stroke",
+                    format!("hsl(200, 100%, {}%", (brightness * 100.0).round()),
+                )
+                .set("stroke-width", 2.0)
+                .set("stroke-linecap", "round"),
+        );
+    }
+
+    Ok(svg::save(file, &doc)?)
 }
