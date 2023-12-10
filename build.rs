f5c4182b852d1e045b0e55bc2363221440d859ef
Felix Zwettler <f.zwettler@posteo.de>
2023-12-10 11:34:09 +0100

    chore: update ink-stroke-modeler, absl, cargo deps

--- build.rs
+++ build.rs
@@ -8,15 +8,15 @@ fn main() -> miette::Result<()> {
     let install_lib_dir = out_dir.join("lib");
     let install_include_dir = out_dir.join("include");
 
-    let bindings_files = vec![
+    let bindings_files = [
         PathBuf::from("build.rs"),
         PathBuf::from("src/lib.rs"),
         PathBuf::from("include/extras.h"),
     ];
-    let bindings_cpp_sources = vec![PathBuf::from("src/extras.cc")];
+    let bindings_cpp_sources = [PathBuf::from("src/extras.cc")];
 
     let _absl_cmake_install_dir = cmake::Config::new("abseil-cpp")
-        // this avoids having to link to `dbghelp` on windows-mingw
+        // This avoids having to link to `dbghelp` on windows-mingw
         .profile("Release")
         .define("CMAKE_CXX_STANDARD", "20")
         // Rust needs -fPIE or -fPIC
@@ -37,13 +37,13 @@ fn main() -> miette::Result<()> {
         .define("BUILD_TESTING", "OFF")
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("ABSL_PROPAGATE_CXX_STD", "ON")
-        // this forces absl stdcpp waiter implementation (see `absl/synchronization/internal/waiter.h`).
-        // this possibly circumvents build failure with mingw. see: https://github.com/abseil/abseil-cpp/issues/1510
+        // This forces absl stdcpp waiter implementation (see `absl/synchronization/internal/waiter.h`).
+        // It possibly circumvents build failure with mingw. see: https://github.com/abseil/abseil-cpp/issues/1510
         .cxxflag("-DABSL_FORCE_WAITER_MODE=4")
         .build();
 
     let _ink_stroke_modeler_cmake_install_dir = cmake::Config::new("ink-stroke-modeler")
-        // this avoids having to link to `dbghelp` on windows-mingw
+        // This avoids having to link to `dbghelp` on windows-mingw
         .profile("Release")
         .define("CMAKE_CXX_STANDARD", "20")
         // Rust needs -fPIE or -fPIC
@@ -68,7 +68,7 @@ fn main() -> miette::Result<()> {
         .define("INK_STROKE_MODELER_ENABLE_INSTALL", "ON")
         .build();
 
-    let include_paths = vec![
+    let include_paths = [
         PathBuf::from("include"),
         //PathBuf::from("absl-cpp"),
         //PathBuf::from("ink-stroke-modeler"),
@@ -78,11 +78,12 @@ fn main() -> miette::Result<()> {
     let mut builder =
         autocxx_build::Builder::new(PathBuf::from("src/lib.rs"), include_paths.iter())
             .extra_clang_args(&["-std=gnu++20"])
-            .build()?;
+            .build()
+            .into_diagnostic()?;
     builder
         //.flag_if_supported("-v")
         .flag_if_supported("-std=gnu++20")
-        // include paths already passed in by the autocxx builder
+        // These include paths are already passed in by the autocxx builder
         //.includes(include_paths.iter())
         //.cpp_set_stdlib(Some("stdc++"))
         .files(bindings_cpp_sources.iter())
