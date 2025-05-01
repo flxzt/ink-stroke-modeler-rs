1f38c2c0de379a49369a362a91fc4fded61da377
flxzt <flxzt@users.noreply.github.com>
2025-05-01 12:49:59 +0000

    deploy: 389da2ba43c1e32bb6bae1e11860196bc87488e8

--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["ink_stroke_modeler_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"ink_stroke_modeler_rs/enum.ModelerInputEventType.html\" title=\"enum ink_stroke_modeler_rs::ModelerInputEventType\">ModelerInputEventType</a>"]]]]);
+    var implementors = Object.fromEntries([["ink_stroke_modeler_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"ink_stroke_modeler_rs/enum.ModelerInputEventType.html\" title=\"enum ink_stroke_modeler_rs::ModelerInputEventType\">ModelerInputEventType</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
